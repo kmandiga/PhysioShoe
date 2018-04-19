8436241508e67ab8cddbbcb43d00e4e6346c702e
kmandiga <kmandiga@uh.edu>
2018-04-19 10:47:04 -0500

    Bluetooth Testing

--- BluetoothSerialESP32/BluetoothSerialESP32.ino
+++ BluetoothSerialESP32/BluetoothSerialESP32.ino
@@ -7,21 +7,61 @@ BluetoothSerial SerialBT;
 
 int packetSize = 61;
 uint8_t serialPacket[61];
+uint8_t command[20] = {' ', ' ', ' ', ' ', ' ',
+                    ' ', ' ', ' ', ' ', ' ',
+                    ' ', ' ', ' ', ' ', ' ',
+                    ' ', ' ', ' ', ' ', '\0'
+                   };
+int numBytes = 0;
+bool tempFlag = false;
 
 void setup() {
-  SerialBT.begin("PhysioShoeRS"); //Device Name RS = Right Shoe
+  Serial.begin(115200);
+  SerialBT.begin("PhysioShoe"); //Device Name LS = Left Shoe
   setupPacket();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //send packet
-  if(SerialBT.available() > 0)
+
+  recieveData();
+  delay(1000);
+  if(tempFlag)
   {
-    Serial.println(SerialBT.read());
+    sendData();
   }
 
+
+}
+void recieveData()
+{
+    /*
+   * Timing is super important for properly recieving
+   * all data. If too little time is given, data sent
+   * gets split up. If too much time is given, data is
+   * pooled together. (if 5 bytes are sent multiple 
+   * times to quickly and delay is too large
+   * it might store 10 bytes)
+   */
+    numBytes = SerialBT.available();
+    if (numBytes <= 20 && numBytes > 0)
+    {
+      for (int i = 0; i < numBytes; i++)
+      {
+        command[i] = SerialBT.read();
+      }
+      for (int i = 18; i >= numBytes; i--)
+      {
+        command[i] = ' ';
+      }
+      Serial.println((char*) command);
+      numBytes = 0;
+      tempFlag = true;
+    }
+}
+void sendData()
+{
+  SerialBT.write(command, 20);
 }
 void setupPacket() {//formats the packet that will be transferred via bluetooth
   for (int i = 0; i < packetSize; i++)
